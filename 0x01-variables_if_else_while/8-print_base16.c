e47b637a19eb11d403ccb42b12dd47e55b76b743
Collins Sande <coliwafula@gmail.com>
2023-02-21 00:58:56 +0300

    Print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Entry point.
+  *
+  * Return: 0 on success
+  */
+int main(void)
+{
+	char an;
+
+	for (an = '0'; an <= '9'; an++)
+	{
+		putchar(an);
+	}
+	for (an = 'a'; an <= 'f'; an++)
+        {
+        	putchar(an);
+ 	}
+	putchar('\n');
+	return (0);
+}
