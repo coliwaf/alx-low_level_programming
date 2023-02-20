4729b01b6660082b66fc80ee69aa905bee523114
Collins Sande <coliwafula@gmail.com>
2023-02-21 02:29:45 +0300

    print combo 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,17 +9,17 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = '0'; i <= 99; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = '0'; j <= 99; j++)
+		for (j = 0; j <= 99; j++)
 		{
 			if (i < j && i != j)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
