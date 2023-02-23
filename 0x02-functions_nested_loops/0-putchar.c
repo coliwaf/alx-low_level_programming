199d83d1af774a97088c60c0bea74dbf48d5392a
Collins Sande <coliwafula@gmail.com>
2023-02-23 22:03:53 +0300

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
   * main - Entry point
@@ -16,6 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
