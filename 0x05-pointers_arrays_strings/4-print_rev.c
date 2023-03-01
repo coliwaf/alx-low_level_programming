ab0099cb8146548e69af4dcd765c25fe59933622
Collins Sande <coliwafula@gmail.com>
2023-03-01 22:47:05 +0300

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_rev - prints string in reverse
+  * @s: string
+  *
+  * Returns void
+  */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
