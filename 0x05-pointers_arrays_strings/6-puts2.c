5c88ddc177b0481407d6e12632f35b279155decb
Collins Sande <coliwafula@gmail.com>
2023-03-01 23:48:44 +0300

    print even characters in string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * puts2 - print even chars
+  * @str: string
+  *
+  * Return: void
+  */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
