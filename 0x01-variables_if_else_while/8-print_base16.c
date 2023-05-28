6911a367a04fbf1343a558fc80078c2b0481797d
Collins Sande <coliwafula@gmail.com>
2023-05-28 16:07:00 +0300

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int numb;
+	char chr;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	for (chr = 'a'; chr <= 'f'; chr++)
+		putchar(chr);
+	putchar('\n');
+	return (0);
+}
