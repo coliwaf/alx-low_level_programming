97db8b5dc5bd23af4c181c97e7ffcabce0d89425
Collins Sande <coliwafula@gmail.com>
2023-05-28 20:05:48 +0300

    print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int numb1, numb2;
+
+	for (numb1 = 0; numb1 <= 98; numb1++)
+	{
+		for (numb2 = numb1 + 1; numb2 <= 99; numb2++)
+		{
+			putchar((numb1 / 10) + 48);
+			putchar((numb1 % 10) + 48);
+			putchar(' ');
+			putchar((numb2 / 10) + 48);
+			putchar((num2 % 10) + 48);
+			if (numb1 != 98 || numb2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
