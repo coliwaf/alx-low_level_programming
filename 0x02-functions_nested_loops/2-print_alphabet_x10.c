9a38e6c702cc2d0fa2037ad541f036b62f8260a8
Collins Sande <coliwafula@gmail.com>
2023-05-28 22:51:39 +0300

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 lines of the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char alph;
+	int numb;
+
+	for (numb = 48; numb <= 57; numb++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph)
+		_putchar('\n');
+	}
+}
