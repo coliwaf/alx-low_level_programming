65f0b145385a3bd5ada0e131bf40a62517325ceb
Collins Sande <coliwafula@gmail.com>
2023-05-28 13:21:00 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet, plus new line
+ * Return: 0
+ */
+int main(0)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+		putchar(chr);
+	putchar('\n');
+	return (0);
+}
