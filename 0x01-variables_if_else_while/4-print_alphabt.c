e4239742a2597cdf44ef3202658187826e503c05
Collins Sande <coliwafula@gmail.com>
2023-05-28 14:33:50 +0300

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		if (chr != 'e' || chr != 'q')
+		{
+			putchar (chr);
+		}
+	}
+	putchar("\n");
+	return (0);
+}
+
