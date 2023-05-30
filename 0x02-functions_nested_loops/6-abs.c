7e2330f856af8cf2f4a0677d471eca70fd72d0f3
Collins Sande <coliwafula@gmail.com>
2023-05-30 20:20:11 +0300

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute val.of an integer
+ * @i: the integer
+ * Return: absolute val of integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	return (-i);
+}
