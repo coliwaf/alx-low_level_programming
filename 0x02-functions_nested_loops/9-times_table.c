685ba04aaa02a5b2c6f8c8b42ae48c4370e11e89
Collins Sande <coliwafula@gmail.com>
2023-05-31 18:54:08 +0000

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,23 +8,25 @@ void times_table(void)
 {
 	int k, l, m;
 
-	for (k = 0; k <= 9; k++)
+	for (k = 0; k < 10; k++)
 	{
 		_putchar('0');
-		for (l = 0; l <= 9; l++)
+		for (l = 1; l < 10; l++)
 		{
 			m = k * l;
-			_putchar(',');
-			_putchar(' ');
-			if (m <= 9)
+			if (m < 10)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(m + '0');
 			}
 			else
 			{
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
 			}
 		}
 		_putchar('\n');
