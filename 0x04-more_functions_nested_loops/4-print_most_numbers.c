ed1d227a7d5f8922e0bc3f8ffc39247846a08e4b
Collins Sande <coliwafula@gmail.com>
2023-06-01 11:08:57 +0000

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,19 @@
 
 /**
  * print_most_numbers - return 0-9 except 2 and 4
- * Return: n/a
+ * Return: 0
  */
 
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 48; i <= 57; i++)
 	{
 		if (i != 50 && i != 52)
-		       _putchar(i);
+		{
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
-}	
+}
