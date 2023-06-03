4b0f18dd59d194c9ec1541b65964badd3287db2e
Collins Sande <coliwafula@gmail.com>
2023-06-03 17:27:49 +0000

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string param
+ */
+
+void rev_string(char *s)
+{
+	int idx, len, temp;
+
+	idx = 0;
+	len = 0;
+	while(s[len] != '\0')
+		len++;
+	len--;
+
+	while (len > idx)
+	{
+		temp = s[len];
+		s[len--] = s[idx];
+		s[idx++] = temp;
+	}
+}	
