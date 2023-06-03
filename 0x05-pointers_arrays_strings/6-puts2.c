b07ef9706ee762d7b17e8df85deb90e3ce4a2a6f
Collins Sande <coliwafula@gmail.com>
2023-06-03 17:46:01 +0000

    prints odd chars in str

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints even chars in a string
+ * @s: string
+ */
+
+void puts2(char *str)
+{
+	int idx = 0;
+
+	while (str[idx] != '\0')
+	{
+		if (idx % 2 == 0)
+			_putchar(str[idx]);
+		idx++;
+	}
+	_putchar('\n');
+}
