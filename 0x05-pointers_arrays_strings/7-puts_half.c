22d2382fe7b5f78e652ef48fc03cfefdae50ae18
Collins Sande <coliwafula@gmail.com>
2023-06-03 19:58:02 +0000

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: str param
+ */
+void puts_half(char *str)
+{
+	int idx, len, n;
+
+	idx = 0;
+	len = 0;	
+	while (str[idx++])
+		len++;
+
+	if ((len % 2) == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
+
+	for (idx = n; idx < len; idx++)
+		_putchar(str[idx]);
+	_putchar('\n');
+}
