25ce9ca0cd17575eb6dd255558d89284e990d732
Collins Sande <coliwafula@gmail.com>
2023-06-05 04:10:58 +0300

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words
+ * @s: string param
+ * Return: pointer to string
+ */
+
+char cap_string(char *s)
+{
+	int idx = 0;
+
+	while (s[idx])
+	{
+		while (!(s[idx] >= 'a' && s[idx] <= 'z'))
+		{
+			idx++;
+		}
+		if(idx == 0 || s[idx - 1] == ' ' || s[idx - 1] == '\t' || s[idx - 1] == '\n' || s[idx - 1] == ',' || s[idx - 1] == ';' || s[idx - 1] == '.' || s[idx - 1] == '!' || s[idx - 1] == '?' || s[idx - 1] == '"' || s[idx - 1] == '(' || s[idx - 1] == ')' || s[idx - 1] == '{' || s[idx -1] == '}')
+		{
+			s[idx] -= 32;
+		}
+		idx++;
+	}
+	return (s);
+}
