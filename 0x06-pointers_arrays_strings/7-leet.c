0498551bfbaca43b0d477446ce12a9e5173f6f9b
Collins Sande <coliwafula@gmail.com>
2023-06-05 04:51:06 +0300

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encodes string into 1337
+ * @s: string param
+ * Return: pointer to str
+ */
+
+char *leet(char s)
+{
+	int i, j;
+	int s1[] = {97, 101, 111, 116, 108};
+	int s2[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
+
+	i = 0;
+
+	while (s[0] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == s1[j] || s[i] == s2[j])
+			{
+				s[i] = num[i];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
