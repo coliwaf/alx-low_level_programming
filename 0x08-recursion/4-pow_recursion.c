5bb480af5d3e641d5fd92309533f9641dc5b2cda
Collins Sande <coliwafula@gmail.com>
2023-06-17 16:53:01 +0000

    power of numbers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the val of x raised to power of y
+ * @x: int param one
+ * @y: int param two
+ * Return: power of @x and @y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y =0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(y -1));
+}
