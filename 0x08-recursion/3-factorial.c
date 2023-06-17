11e3abdaef92e76aa96ab2920ca249653ae44bbf
Collins Sande <coliwafula@gmail.com>
2023-06-17 16:43:31 +0000

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,8 +4,8 @@
  * factorial - returns the factorial of a number
  * @n: integer param
  * Return: -1 error if @n is lower than 0
- * 	   1 if @n is 0
- * 	   if success return factorial of @n
+ * 1 if @n is 0
+ * if success return factorial of @n
  */
 int factorial(int n)
 {
