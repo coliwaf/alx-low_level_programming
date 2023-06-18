2cb6bdbfc6824b0c61120357216ce7dd9227e945
Collins Sande <coliwafula@gmail.com>
2023-06-18 14:43:41 +0000

    atoi-main_h

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,12 +9,12 @@ int _atoi(char *s)
 {
 	int i;
 	int result = 0;
-	int sign = -1;
+	int sign = 1;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
-			sign *= -1;
+			sign = sign * -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = (result * 10) + (s[i] - '0');
