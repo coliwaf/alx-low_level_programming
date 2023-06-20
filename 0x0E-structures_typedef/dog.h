ec61cf0bb1f5788f651381fee9c8c1381b55d9b2
Collins Sande <coliwafula@gmail.com>
2023-06-20 02:22:14 +0000

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - name of the struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
