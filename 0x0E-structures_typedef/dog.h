c72398551ad0133ab841fbcf01c7ff947d36be07
Collins Sande <coliwafula@gmail.com>
2023-06-20 01:17:20 +0000

    init dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines dog object
+ * @name: dog's name
+ * @age: dog's edge
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
