0abf007981bea50c96c20a0d0e80127a5b9fb3bf
Collins Sande <coliwafula@gmail.com>
2023-06-22 11:47:50 +0000

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - malloc allocates memory or exits if failed
+ * @b: quantity of bytes to allocate
+ * Returns: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr_alloc;
+
+	ptr_alloc = malloc(b);
+
+	if (ptr_alloc == NULL)
+		exit(98);
+	return (ptr_alloc);
+}
