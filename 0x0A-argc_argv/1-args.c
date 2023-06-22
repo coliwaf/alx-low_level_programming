9bea626a232522b44e201f8799c81521ab396cc6
Collins Sande <coliwafula@gmail.com>
2023-06-22 02:58:24 +0000

    prints args passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to it
+ * @argc: number of arguments
+ * @argv: array of arguments param
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
