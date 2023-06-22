2190b96177bfdf9b9bfaf86d3e4b29c5d362a766
Collins Sande <coliwafula@gmail.com>
2023-06-22 04:37:54 +0000

    prints required coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,17 +12,16 @@ int main(int argc, char *argv[])
 	int numb, i, res;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	res = 0
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+	res = 0;
 	numb = atoi(argv[1]);
 
-	if (num < 0)
+	if (numb < 0)
 	{
 		printf("0\n");
 		return (0);
@@ -30,10 +29,10 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < 5 && numb >= 0; i++)
 	{
-		while (numb >= coins[j])
+		while (numb >= coins[i])
 		{
 			res++;
-			numb -= coins[j];
+			numb -= coins[i];
 		}
 	}
 
