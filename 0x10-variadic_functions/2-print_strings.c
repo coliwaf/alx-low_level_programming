eec8bbe6dbd9c1187b08ba635af79a328149d441
Collins Sande <coliwafula@gmail.com>
2023-06-23 00:26:04 +0000

    print strings 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 
 	va_start(arglist, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(arglist, char*);
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("(nil)");
 
-		if(separator && i < (n - 1))
+		if (separator && i < (n - 1))
 			printf("%s", separator);
 	}
 	va_end(arglist);
