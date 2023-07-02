bc9b4e5a54ef43475aa3a0ecb1fb6a295864cb42
Collins Sande <coliwafula@gmail.com>
2023-07-02 14:16:49 +0000

    task 3 - small program

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects correct function to perform as needed by user
+ * @s: operator chacter given by user
+ * Return: pointer to function corresponding to the @s operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculate sum of two integers
+ * @a: first integer param
+ * @b: second integer param
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates difference between two integers
+ * @a: first integer param
+ * @b: second integer param
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculate multiplication of two integers
+ * @a: first integer param
+ * @b: second integer param
+ * Return: multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculate division btwn two integers
+ * @a: first integer param
+ * @b: second integer param
+ * Return: a divided by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates modulo of two integers
+ * @a: first integer param
+ * @b: second integer param
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
