b34150a0364fd5191b9f57d5a3f646d018d46d5c
Collins Sande <coliwafula@gmail.com>
2023-07-02 14:37:38 +0000

    correcting floating point error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,18 +18,18 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
+	f = (*get_op_func)(argv[2]);
+
+	if (argv[2][1] != '\0' || f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	f = (*get_op_func)(argv[2]);
-
-	if (f == NULL)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	i = atoi(argv[1]);
