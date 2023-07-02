8dde998e71480d192ae6e4ac135a262f4c44aeaf
Collins Sande <coliwafula@gmail.com>
2023-07-02 14:18:31 +0000

    task 3 - small program

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main: calculates math of two integers and an operator 
+ * via a function pointer.
+ * @argc: arguments counter
+ * @argv: list of arguments
+ * Return: 0 always (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	f = (*get_op_func)(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	printf("%d\n", f(i, j));
+	return (0);
+}
