054b8dabeb0f07e3308a1b6e2deb8cd316eec82e
Collins Sande <coliwafula@gmail.com>
2023-08-25 17:46:29 +0000

    binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary-to_uint - func converts a binary to an unsigned int
+ * @b: pointer to string having the binary param
+ * Return: unsigned intin decimal, otherwise return 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decm;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	decm = 0;
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		decm << = 1;
+		if (b[i] == '1')
+			decm += 1;
+	}
+	return (decm);
+}
